ea3d06069834cd9f9e066478bd4dbd6c62ac5170
Shreyansh-32 <thakurshreyansh0609@gmail.com>
2026-05-10 13:01:02 +0530

    Fix issue

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,7 +1,6 @@
 "use client";
 
 import { useState, useEffect } from "react";
-import Link from "next/link";
 import { motion } from "framer-motion";
 import Link from "next/link";
 import { MapPin, Menu, X } from "lucide-react";
